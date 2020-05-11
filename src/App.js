04ad337263499dc8f6e92f0c12c3ca6a70c62afd
Aahad patel <aahadpat@usc.edu>
2020-05-11 00:12:20 -0500

    adding routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Nav from "react-bootstrap/Nav";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 class App extends React.Component {
   constructor(props) {
@@ -42,6 +45,8 @@ class App extends React.Component {
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
+                {/* react router using link component instead of anchor tag or
+                button "to" refers to the path we want to go to */}
                 <Link className="nav-link" to="/">
                   Home
                 </Link>
@@ -54,6 +59,26 @@ class App extends React.Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+          {/* Routes are saying that whenever you are at this path, render this component */}
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <HomePage
+                title={this.state.home.title}
+                subTitle={this.state.home.subTitle}
+                text={this.state.home.text}
+              />
+            )}
+          />
+          <Route
+            path="/about"
+            render={() => <AboutPage title={this.state.about.title} />}
+          />
+          <Route
+            path="/contact"
+            render={() => <ContactPage title={this.state.contact.title} />}
+          />
           <Footer />
         </Container>
       </Router>
